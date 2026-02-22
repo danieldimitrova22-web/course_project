0ae5b24dccb3fbc253e5b66445f69d29ffb606cc
danieldimitrova22-web <danieldimitrov_a22@schoolmath.eu>
2026-02-22 11:02:03 +0200

    Add files via upload

---
+++ BusinessLayer/Genre.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessLayer;
+
+public class Genre
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(20, ErrorMessage = "Name must be max 20 symbols!")]
+	public string Name { get; set; }
+
+	public List<Book> Books { get; set; }
+
+	public Genre()
+	{
+		Books = new List<Book>();
+	}
+
+	public Genre(string name)
+	{
+		Name = name;
+		Books = new List<Book>();
+	}
+
+}

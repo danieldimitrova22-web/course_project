50dc570918eeed8c64a425ddc2e39ded967e0d9c
danieldimitrova22-web <danieldimitrov_a22@schoolmath.eu>
2026-02-22 11:10:14 +0200

    Update Platforms.cs

--- Business layer/Platforms.cs
+++ Business layer/Platforms.cs
@@ -8,5 +8,43 @@ namespace Business_layer
 {
 	public class Platforms
 	{
+		[Key]
+
+		public int Id { get; private set; }
+		
+		[Required]
+		[MaxLength(50)]
+		
+		public string Name { get; private set; }
+		
+		[Required]
+		[MaxLength(40)]
+		public string Manufacturer { get; private set; }
+		
+		[Required]
+		public string Release_date { get; private set; }
+		
+		[Required]
+		public string Generation { get; private set; }
+		
+		
+		
+		
+		
+		public List<VideoGame> VideoGames { get; private set; }
+		
+		private Platform()
+		{
+			
+		}
+		
+		public Platform(string name, string manufacturer, string release_date, string generation) : this()
+		{
+			this.Name = name;
+			this.Manufacturer = manufacturer;
+			this.Release_date = release_date;
+			this.Generation = generation;
+		}
 	}
 }
+
